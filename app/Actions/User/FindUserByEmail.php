cc1c607df373be181b966e36e55de7ae64659b92
Marco Guidolin <m.guidolin@egeatech.com>
2023-11-26 19:36:18 +0100

    ADD user retrieval action

---
+++ app/Actions/User/FindUserByEmail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Actions\User;
+
+use App\Models\User;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+
+class FindUserByEmail
+{
+    /**
+     * @var Builder<User>
+     */
+    private readonly Builder $userQuery;
+
+    public function __construct()
+    {
+        $this->userQuery = User::query();
+    }
+
+    public function execute(string $email, bool $failIfNotFound = false): ?User
+    {
+        $fetchQuery = $this->userQuery
+            ->where('email', '=', $email);
+
+        return $failIfNotFound
+            ? $fetchQuery->firstOrFail()
+            : $fetchQuery->first();
+    }
+}
