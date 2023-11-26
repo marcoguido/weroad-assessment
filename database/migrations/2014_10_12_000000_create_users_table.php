26f7e1f505573ad04bb273e817729410e2daa033
Marco Guidolin <m.guidolin@egeatech.com>
2023-11-26 19:35:08 +0100

    UPDATE user migrations
    Now supporting also a `name` field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('email')->unique();
+            $table->string('name');
             $table->string('password');
             $table->timestamp(User::CREATED_AT)->nullable();
             $table->timestamp(User::UPDATED_AT)->nullable();
