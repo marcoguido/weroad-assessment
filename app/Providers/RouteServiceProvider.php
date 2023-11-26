5d9c20f5da390ad176b153376dd341e112b7c882
Marco Guidolin <m.guidolin@egeatech.com>
2023-11-26 19:40:21 +0100

    ADD model identifiers to route parameter binders, in order to use ValueObjects as controller parameters

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,14 +2,26 @@
 
 namespace App\Providers;
 
+use App\Contracts\Models\Identifiers\IdentifierContract;
+use App\Models\Identifiers\TourId;
+use App\Models\Identifiers\TravelId;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    /**
+     * @var class-string[]
+     */
+    private static array $routeIdentifierClasses = [
+        TourId::class,
+        TravelId::class,
+    ];
+
     /**
      * The path to your application's "home" route.
      *
@@ -24,10 +36,34 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->setupRateLimiter();
+        $this->setupRouteBindings();
+        $this->registerRoutes();
+    }
+
+    private function setupRateLimiter(): void
+    {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+    }
 
+    private function setupRouteBindings(): void
+    {
+        /** @var IdentifierContract $identifierClass */
+        foreach (static::$routeIdentifierClasses as $identifierClass) {
+            Route::bind(
+                key: Str::of($identifierClass)
+                    ->classBasename()
+                    ->camel()
+                    ->toString(),
+                binder: fn (string $value) => $identifierClass::make($value),
+            );
+        }
+    }
+
+    private function registerRoutes(): void
+    {
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
