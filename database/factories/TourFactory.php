5d24bee0f04a0133dfc0cb2632dd8582f0b07c09
Marco Guidolin <m.guidolin@egeatech.com>
2023-11-27 08:41:37 +0100

    ADD models factories

---
+++ database/factories/TourFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tour;
+use Carbon\CarbonImmutable;
+use DateTime;
+use Exception;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+/**
+ * @extends Factory<Tour>
+ */
+class TourFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     * @throws Exception
+     */
+    public function definition(): array
+    {
+        $tourStartingDate = CarbonImmutable::parse(
+            $this->faker->dateTimeBetween(startDate: '-2 years', endDate: '+2 years')
+        );
+        $tourEndingDate = $tourStartingDate->add(
+            unit: $this->faker->randomElement(['day', 'week']),
+            value: $this->faker->numberBetween(1, 5),
+        );
+
+        return [
+            'name' => $this->faker->sentence(),
+            'startingDate' => $tourStartingDate->format(Tour::DATE_FORMAT),
+            'endingDate' => $tourEndingDate->format(Tour::DATE_FORMAT),
+            'price' => $this->faker->numberBetween(int1: 100),
+        ];
+    }
+}
---
+++ database/factories/TravelFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Travel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Travel>
+ */
+class TravelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'isPublic' => $this->faker->boolean(),
+            'name' => $this->faker->sentence(),
+            'description' => $this->faker->realText(),
+            'numberOfDays' => $this->faker->numberBetween(1, 20),
+            'moods' => $this->makeRandomMoods(
+                $this->faker->numberBetween(1, 10)
+            ),
+        ];
+    }
+
+    private function makeRandomMoods(int $amount): array
+    {
+        $moods = [];
+        for ($moodIndex = 0 ; $moodIndex < $amount ; $moodIndex++) {
+            // Ensure no mood is generated twice
+            do {
+                $moodName = $this->faker->word();
+            } while (array_key_exists(key: $moodName, array: $moods));
+            $moodPercentage = $this->faker->numberBetween(1, 100);
+            $moods[$moodName] = $moodPercentage;
+        }
+
+        return $moods;
+    }
+}
