89878b6b43a9ccd7536e1ce70b4f5ba0f6553a8f
Marco Guidolin <m.guidolin@egeatech.com>
2023-11-28 01:42:12 +0100

    UPDATE travel creation API, better variable type hinting

--- app/Http/Controllers/Api/v1/Travels/CreateTravelController.php
+++ app/Http/Controllers/Api/v1/Travels/CreateTravelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1\Travels;
 use App\Actions\Travel\StoreTravel;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\v1\Travels\CreateTravelRequest;
+use App\Http\Requests\Api\v1\Travels\Data\TravelData;
 use App\Http\Responses\Api\V1\Resources\TravelResource;
 use Illuminate\Http\JsonResponse;
 use Spatie\LaravelData\Exceptions\InvalidDataClass;
@@ -16,9 +17,9 @@ class CreateTravelController extends Controller
      */
     public function __invoke(CreateTravelRequest $request, StoreTravel $storeAction): JsonResponse
     {
-        $newTravel = $storeAction->execute(
-            $request->getData()
-        );
+        /** @var TravelData $travelData */
+        $travelData = $request->getData();
+        $newTravel = $storeAction->execute($travelData);
 
         return new JsonResponse(
             data: TravelResource::from($newTravel->refresh()),
