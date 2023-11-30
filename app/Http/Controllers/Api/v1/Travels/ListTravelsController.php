0f88f232e373795af35d69dbc84a90c119e8907c
Marco Guidolin <m.guidolin@egeatech.com>
2023-11-30 10:04:01 +0100

    ADD GET Travels APIs documentation

--- app/Http/Controllers/Api/v1/Travels/ListTravelsController.php
+++ app/Http/Controllers/Api/v1/Travels/ListTravelsController.php
@@ -5,14 +5,59 @@ namespace App\Http\Controllers\Api\v1\Travels;
 use App\Actions\Travel\GetPaginatedTravels;
 use App\Http\Controllers\Controller;
 use App\Http\Responses\Api\V1\Resources\TravelResource;
+use App\OpenApi\Responses\Travels\ListTravelsResponse;
+use App\OpenApi\SecuritySchemes\TokenSecurityScheme;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
 
+#[OpenApi\PathItem]
 class ListTravelsController extends Controller
 {
-    public function __invoke(GetPaginatedTravels $fetchAction): JsonResponse
+    public function __construct(
+        private readonly GetPaginatedTravels $fetchAction,
+    ) {
+    }
+
+    /**
+     * Gets all available travels
+     *
+     * Only when the API is invoked by an authenticated user private
+     * travels can be retrieved
+     */
+    #[OpenApi\Operation(
+        tags: ['Travel'],
+        security: TokenSecurityScheme::class,
+        method: 'GET',
+    )]
+    #[OpenApi\Response(
+        factory: ListTravelsResponse::class,
+        statusCode: JsonResponse::HTTP_OK,
+    )]
+    public function asAdministrator(): JsonResponse
     {
-        $travels = $fetchAction->execute(onlyPublic: ! Auth::check());
+        return $this->fetchTravels(onlyPublic: ! Auth::check());
+    }
+
+    /**
+     * Gets all publicly available travels
+     */
+    #[OpenApi\Operation(
+        tags: ['Travel'],
+        method: 'GET',
+    )]
+    #[OpenApi\Response(
+        factory: ListTravelsResponse::class,
+        statusCode: JsonResponse::HTTP_OK,
+    )]
+    public function publiclyAvailable(): JsonResponse
+    {
+        return $this->fetchTravels(onlyPublic: true);
+    }
+
+    private function fetchTravels(bool $onlyPublic): JsonResponse
+    {
+        $travels = $this->fetchAction->execute($onlyPublic);
 
         return new JsonResponse(
             data: TravelResource::collection($travels),
---
+++ app/OpenApi/Responses/Travels/ListTravelsResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\OpenApi\Responses\Travels;
+
+use App\OpenApi\Schemas\Pagination\PaginationLinksSchema;
+use App\OpenApi\Schemas\Pagination\PaginationMetaSchema;
+use App\OpenApi\Schemas\Tours\TourOutputSchema;
+use App\OpenApi\Schemas\Travels\TravelOutputSchema;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+use Vyuldashev\LaravelOpenApi\Factories\ResponseFactory;
+
+class ListTravelsResponse extends ResponseFactory
+{
+    public function build(): Response
+    {
+        return Response::ok()
+            ->description('Available travels')
+            ->content(
+                MediaType::json()->schema(
+                    Schema::object()->properties(
+                        Schema::array('data')->items(TravelOutputSchema::ref()),
+                        PaginationLinksSchema::ref('links'),
+                        PaginationMetaSchema::ref('meta'),
+                    ),
+                ),
+            );
+    }
+}
