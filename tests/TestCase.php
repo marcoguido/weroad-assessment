8dba337e059001986526ac0f2939598b32a6638a
Marco Guidolin <m.guidolin@egeatech.com>
2023-12-12 08:31:01 +0100

    ADD seeder to be executed by test executor on database refresh

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,14 @@
 
 namespace Tests;
 
+use Database\Seeders\DevelopmentSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    // Automatically invokes development seeder
+    // when refreshing database
+    protected string $seeder = DevelopmentSeeder::class;
 }
