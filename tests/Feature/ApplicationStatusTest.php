dede8e438506be80fe34f923511dfbbe595ec67d
Marco Guidolin <m.guidolin@egeatech.com>
2023-12-13 09:12:54 +0100

    ADD test to check for OpenAPI specs file availability

--- tests/Feature/ApplicationStatusTest.php
+++ tests/Feature/ApplicationStatusTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\File;
+
 it(
     'test the application is running a successful response',
     fn () => $this->get('/')->assertOk(),
@@ -8,4 +10,9 @@ it(
 it(
     'test Swagger editor is available',
     fn () => $this->get('/swagger')->assertOk(),
+);
+
+it(
+    'test OpenApi JSON file is available',
+    fn () => expect(File::exists(public_path('openapi.json')))->toBeTrue(),
 );
