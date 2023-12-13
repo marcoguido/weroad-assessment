0275c779c7d6bbcef085da12652cfa8ef173aee2
Marco Guidolin <m.guidolin@egeatech.com>
2023-12-13 18:33:45 +0100

    ADD public tours retrieval tests
    Generic listing test and sorting test

---
+++ tests/Feature/TourPublicApisTest.php
@@ -0,0 +1,90 @@
+<?php
+
+use App\Models\Travel;
+use Illuminate\Support\Carbon;
+use Tests\Feature\Constants\RouteName;
+
+it(
+    'test that all tours of a public travel can be retrieved and they are sorted by starting date ASC',
+    function () {
+        /** @var Travel $travel */
+        $travel = Travel::query()
+            ->whereHas('tours')
+            ->where('isPublic', '=', true)
+            ->first();
+
+        $apiResponse = $this
+            ->get(
+                uri: route(
+                    name: RouteName::PUBLIC_TRAVEL_TOURS_INDEX->value,
+                    parameters: [
+                        'travel' => $travel->slug,
+                        'page[size]' => PHP_INT_MAX, // Asking for a *really* big result page
+                    ],
+                ),
+            )
+            ->assertSuccessful()
+            ->assertJsonIsObject();
+
+        expect($apiResponse->json())
+            ->data->toBeArray()
+            ->links->toBeArray()
+            ->meta->toBeArray();
+
+
+        $originalWsTours = $apiResponse->json('data');
+        $wsToursSortedByDate = collect($apiResponse->json('data'))
+            ->sortBy(
+                fn (array $tourData) => Carbon::parse($tourData['startingDate'])->timestamp,
+            )
+            ->pluck('id');
+
+        foreach ($wsToursSortedByDate as $index => $sortedTour) {
+            expect($originalWsTours[$index]['id'])->toBe($sortedTour);
+        }
+    },
+);
+
+it(
+    'test that all tours of a public travel can be retrieved and they can sorted by price',
+    function (string $sortingDirection) {
+        /** @var Travel $travel */
+        $travel = Travel::query()
+            ->whereHas('tours')
+            ->where('isPublic', '=', true)
+            ->first();
+
+        $apiResponse = $this
+            ->get(
+                uri: route(
+                    name: RouteName::PUBLIC_TRAVEL_TOURS_INDEX->value,
+                    parameters: [
+                        'travel' => $travel->slug,
+                        'sort' => "{$sortingDirection}price",
+                        'page[size]' => PHP_INT_MAX, // Asking for a *really* big result page
+                    ],
+                ),
+            )
+            ->assertSuccessful()
+            ->assertJsonIsObject();
+
+        expect($apiResponse->json())
+            ->data->toBeArray()
+            ->links->toBeArray()
+            ->meta->toBeArray();
+
+
+        $originalWsTours = $apiResponse->json('data');
+        $wsToursSortedByDate = collect($apiResponse->json('data'))
+            ->sortBy(
+                callback: 'price',
+                descending: $sortingDirection === '-',
+            )
+            ->pluck('price');
+
+        foreach ($wsToursSortedByDate as $index => $sortedTour) {
+            // Comparing `prices` as there may be equally-priced tours
+            expect($originalWsTours[$index]['price'])->toBe($sortedTour);
+        }
+    },
+)->with('sortingDirections');
