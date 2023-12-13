721376b66ae9251ab02afa20a40882adf8f7fa80
Marco Guidolin <m.guidolin@egeatech.com>
2023-12-13 16:48:31 +0100

    ADD test to check private travels unavailability in public API response

--- tests/Feature/TravelPublicApisTest.php
+++ tests/Feature/TravelPublicApisTest.php
@@ -6,8 +6,39 @@ it(
     'test that all public travels can be retrieved',
     function () {
         $apiResponse = $this
-            ->get(static::$PUBLIC_TRAVEL_INDEX_API)
+            ->get(static::$PUBLIC_TRAVELS_API_PATH)
             ->assertSuccessful()
             ->assertJsonIsObject();
+
+        expect($apiResponse->json())
+            ->data->toBeArray()
+            ->links->toBeArray()
+            ->meta->toBeArray();
+    },
+);
+
+it(
+    'test that private travels cannot be retrieved via public API',
+    function () {
+        // Creating a new private travel, to make
+        // sure we have at least one of them
+        Travel::factory()->private()->createOne();
+
+        // Fetching ALL available travels via public API
+        // in order to check that private ones are
+        // unavailable
+        $apiResponse = $this
+            ->get(
+                static::$PUBLIC_TRAVELS_API_PATH.'?page[size]='.PHP_INT_MAX, // Asking for a *really* big result page
+            )
+            ->assertSuccessful()
+            ->assertJsonIsObject();
+
+        $privateTravel = collect($apiResponse->json('data'))
+            ->first(
+                fn (array $travelResponseObject) => ! ((bool) $travelResponseObject['isPublic']),
+            );
+
+        expect($privateTravel)->toBeNull();
     },
 );
