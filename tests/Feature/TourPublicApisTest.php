23b2dee08c827154f0ea67d315bffec1c27c99c0
Marco Guidolin <m.guidolin@egeatech.com>
2023-12-14 09:36:35 +0100

    ADD tour listing testing for price filters

--- tests/Feature/TourPublicApisTest.php
+++ tests/Feature/TourPublicApisTest.php
@@ -88,3 +88,54 @@ it(
         }
     },
 )->with('sortingDirections');
+
+it(
+    'test that public tours of a travel can be filtered by baseline price',
+    function (string $filterName) {
+        /** @var Travel $travel */
+        $travel = Travel::query()
+            ->with('tours')
+            ->whereHas('tours')
+            ->where('isPublic', '=', true)
+            ->first();
+        $randomTour = $travel->tours->random();
+
+        // Perform API call and save the response
+        $apiResponse = $this
+            ->get(
+                uri: route(
+                    name: RouteName::PUBLIC_TRAVEL_TOURS_INDEX->value,
+                    parameters: [
+                        'travel' => $travel->slug,
+                        "filter[$filterName]" => $randomTour->price,
+                        'page[size]' => PHP_INT_MAX, // Asking for a *really* big result page
+                    ],
+                ),
+            )
+            ->assertSuccessful()
+            ->assertJsonIsObject();
+
+        // Check response structure compliance
+        expect($apiResponse->json())
+            ->data->toBeArray()
+            ->links->toBeArray()
+            ->meta->toBeArray();
+
+        // Check that in WS response there are ONLY tours matching the filtering..
+        $filteringOperator = $filterName === 'priceFrom'
+            ? '>='
+            : '<=';
+        $wsToursMatchingFiltering = collect($apiResponse->json('data'))
+            ->where('price', $filteringOperator, $randomTour->price)
+            ->pluck('price')
+            ->count();
+        expect($wsToursMatchingFiltering)->toBe(count($apiResponse->json('data')));
+
+        // ...And that they are the same amount of DB ones, when applying the same filtering
+        $filteredDatabaseTours = $travel->tours
+            ->where('price', $filteringOperator, $randomTour->price)
+            ->count();
+        expect($filteredDatabaseTours)->toBe($wsToursMatchingFiltering);
+    },
+)->with('pricingFilters');
+
